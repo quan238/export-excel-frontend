160a02f19d23e2045f1c1c394c1f5a44ffc0a331
Quan Tran <quan.tran@fullertonhealth.com>
2022-03-26 22:01:54 +0700

    bug fix deploy

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -1,10 +1,10 @@
 // import './App.scss';
-import { Router, Route, Switch, Redirect } from "react-router-dom";
-import HomePage from 'pages/HomePage'
-import 'assets/scss/App.scss'
-import history from "helper/history";
-import HomeTemplate from "template/HomeTemplate";
-import { routes } from "routes/routes";
+import { Router, Route, Switch, Redirect } from 'react-router-dom';
+import HomePage from 'pages/HomePage';
+import '../assets/scss/App.scss';
+import history from 'helper/history';
+import HomeTemplate from 'template/HomeTemplate';
+import { routes } from 'routes/routes';
 
 // styled
 import 'antd/dist/antd.css';
@@ -15,12 +15,7 @@ const renderRoute = (Component, routes) => {
   if (routes && routes.length > 0) {
     return routes.map((item, index) => {
       return (
-        <Component
-          key={index}
-          exact={item.exact}
-          path={item.path}
-          Component={item.component}
-        />
+        <Component key={index} exact={item.exact} path={item.path} Component={item.component} />
       );
     });
   }
